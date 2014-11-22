3909b16684b697f75993d514dd76e020369a27f0
amstil <swash87@mail.ru>
2014-11-22 21:33:56 +0300

    [Logic] added sidebar right elements to MainPage class

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -11,79 +11,123 @@ import java.util.List;
  */
 public class MainPage extends Page {
     //Header elements:
-    private WebElement habrLink;
-    private WebElement geektimesLink;
-    private WebElement tosterLink;
-    private WebElement brainstorageLink;
-    private WebElement freelanceLink;
-    private WebElement habrastorageLink;
-    private WebElement simpleSeoLink;
-    private WebElement malinaForAdminLink;
-    private List<WebElement> headerMenu;
-    private List<WebElement> headerSpecialMenu;
+    private By habrLink = By.cssSelector("div#TMpanel div[class='menu'] a:nth-child(1)");
+    private By geektimesLink = By.cssSelector("div#TMpanel div[class='menu'] a:nth-child(2)");
+    private By tosterLink = By.xpath("//div[@id='TMpanel']//div[@class='menu']//a[3]");
+    private By brainstorageLink = By.xpath("//div[@id='TMpanel']//div[@class='menu']//a[4]");
+    private By freelanceLink = By.cssSelector("div#TMpanel div[class='menu'] a:nth-child(5)");
+    private By habrastorageLink = By.cssSelector("div#TMpanel div[class='menu'] a:nth-child(6)");
+    private By simpleSeoLink = By.xpath("//div[@id='TMpanel']//div[@class='menu special']//a[1]");
+    private By specialLinkTwo = By.cssSelector("div#TMpanel div[class='menu special'] a:nth-child(2)");
+    private By headerMenu = By.cssSelector("div#TMpanel div[class='menu'] a");
+    private By headerSpecialMenu = By.cssSelector("div#TMpanel div[class='menu special'] a");
+
+    //Sidebar right menu links Rubriki
+    private By apiLink = By.cssSelector("div#layout div.sidebar_right li:nth-child(1) a");
+    private By administrationLink = By.cssSelector("div#layout div.sidebar_right li:nth-child(2) a");
+    private By dbLink = By.cssSelector("div#layout div.sidebar_right li:nth-child(3) a");
+    private By securityLink = By.cssSelector("div#layout div.sidebar_right li:nth-child(4) a");
+    private By designLink = By.cssSelector("div#layout div.sidebar_right li:nth-child(5) a");
+    private By managementLink = By.cssSelector("div#layout div.sidebar_right li:nth-child(6) a");
+    private By programmingLink = By.cssSelector("div#layout div.sidebar_right li:nth-child(7) a");
+    private By softwareLink = By.cssSelector("div#layout div.sidebar_right li:nth-child(8) a");
+    private By telecommunicationLink = By.cssSelector("div#layout div.sidebar_right li:nth-child(9) a");
+    private By frameworksCMSLink = By.cssSelector("div#layout div.sidebar_right li:nth-child(10) a");
+    private By frontendLink = By.cssSelector("div#layout div.sidebar_right li:nth-child(11) a");
+    private By differentLink = By.cssSelector("div#layout div.sidebar_right li:nth-child(12) a");
+
 
     public MainPage(WebDriver driver) {
         super(driver);
     }
 
     public void clickHabrLink() {
-        habrLink = getDriver().findElement(By.
-                cssSelector("div#TMpanel div[class='menu'] a:nth-child(1)"));
-        habrLink.click();
+        getDriver().findElement(habrLink).click();
     }
 
     public void clickGeektimesLink() {
-        geektimesLink = getDriver().findElement(By.
-                cssSelector("div#TMpanel div[class='menu'] a:nth-child(2)"));
-        geektimesLink.click();
+        getDriver().findElement(geektimesLink).click();
     }
 
     public void clickTosterLink() {
-        tosterLink = getDriver().findElement(By.
-                xpath("//div[@id='TMpanel']//div[@class='menu']//a[3]"));
-        tosterLink.click();
+        getDriver().findElement(tosterLink).click();
     }
 
     public void clickBrainstorageLink() {
-        brainstorageLink = getDriver().findElement(By.
-                xpath("//div[@id='TMpanel']//div[@class='menu']//a[4]"));
-        brainstorageLink.click();
+        getDriver().findElement(brainstorageLink).click();
     }
 
     public void clickFreelanceLink() {
-        freelanceLink = getDriver().findElement(By.id("TMpanel")).
-                findElement(By.cssSelector("div[class='menu'] a:nth-child(5)"));
-        freelanceLink.click();
+        getDriver().findElement(freelanceLink).click();
     }
 
     public void clickHabrastorageLink() {
-        habrastorageLink = getDriver().findElement(By.
-                cssSelector("div#TMpanel div[class=menu] a:nth-child(6)"));
-        habrastorageLink.click();
+        getDriver().findElement(habrastorageLink).click();
     }
 
     public void clickSimpleSeoLink() {
-        simpleSeoLink = getDriver().findElement(By.className("special")).
-                findElement(By.xpath("a[1]"));
-        simpleSeoLink.click();
+        getDriver().findElement(simpleSeoLink).click();
     }
 
     public void clickMalinaForAdminLink() {
-        malinaForAdminLink = getDriver().findElement(By.
-                cssSelector("div#TMpanel div[class='menu special'] a:nth-child(2)"));
-        malinaForAdminLink.click();
+        getDriver().findElement(specialLinkTwo).click();
     }
 
-    public List<WebElement> getHeaderMenu() {
-        return headerMenu;
+    public List<WebElement> getHeaderMenuLinks() {
+        return getDriver().findElements(headerMenu);
+
     }
 
-    public List<WebElement> getHeaderSpecialMenu() {
-        return headerSpecialMenu;
+    public List<WebElement> getHeaderSpecialMenuLinks() {
+        return getDriver().findElements(headerSpecialMenu);
     }
 
-    public List<WebElement> countElementsHeaderMenu() {
-        headerMenu = getDriver().findElements(By.cssSelector("div#TMpanel div[class=menu] a"));
-        return headerMenu;
+
+    public void clickAPILink() {
+        getDriver().findElement(apiLink).click();
+    }
+
+    public void clickAdministrationLink() {
+        getDriver().findElement(administrationLink).click();
+    }
+
+    public void clickDBLink() {
+        getDriver().findElement(dbLink).click();
+    }
+
+    public void clickSecurityLink() {
+        getDriver().findElement(securityLink).click();
+    }
+
+    public void clickDesignLink() {
+        getDriver().findElement(designLink).click();
+    }
+
+    public void clickManagementLink() {
+        getDriver().findElement(managementLink).click();
+    }
+
+    public void clickProgrammingLink() {
+        getDriver().findElement(programmingLink).click();
+    }
+
+    public void clickSoftwareLink() {
+        getDriver().findElement(softwareLink).click();
+    }
+
+    public void clickTelecommunicationLink() {
+        getDriver().findElement(telecommunicationLink).click();
+    }
+
+    public void clickFrameworksCMSLink() {
+        getDriver().findElement(frameworksCMSLink).click();
+    }
+
+    public void clickFrontendLink() {
+        getDriver().findElement(frontendLink).click();
+    }
+
+    public void clickDifferentLink() {
+        getDriver().findElement(differentLink).click();
     }
 }
